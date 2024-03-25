ccccdb6e10f29d0eb57ab38b176a0de3c17da39d
Michael Barrera <soporte@michaelbarrera.dev>
2024-03-25 01:32:28 -0600

    Bug: Nav buttons

--- src/components/Header/header.ts
+++ src/components/Header/header.ts
@@ -3,77 +3,72 @@ const header = document.querySelector("Header");
 const nav = document.querySelector("nav");
 const menuItems = document.querySelector("#menuItems");
 const miniCart = document.querySelector("#miniCart");
-
-const goToMenuButton = header?.querySelector("#goToMenuButton");
-const cartMenuBtn = header?.querySelector("#cartMenuButton");
+const icons = header?.querySelectorAll("svg");
+const modalMenuButton = header?.querySelector("#modalMenuButton");
+const modalCartButton = header?.querySelector("#modalCartButton");
 
 let isOpen = false;
-let openWith = "";
+let isMiniCartOpen = false;
+let isMenuItemsOpen = false;
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         switch (button.id) {
 
             case "menuButton": {
-                const icons = button.querySelectorAll("svg");
-                icons.forEach((icon) => {
-                    icon.classList.toggle("hidden");
-                });
-                toggleNav("menu");
 
-                openWith = "menu";
+                if (!isOpen) {
+                    toggleNavItems(button.id);
+                    setTimeout(() => {
+                        toggleNav();
+                        toggleHammyIcon();
+                    }, 100);
+                } else {
+                    setTimeout(() => {
+                        toggleNavItems(button.id);
+                        toggleMiniCart(button.id);
+                    }, 100);
+                    toggleNav();
+                    toggleHammyIcon();
+
+
+                };
 
                 break;
             }
 
             case "cartButton": {
-                menuItems?.classList.toggle("hidden")
-                miniCart?.classList.toggle("hidden")
 
                 if (!isOpen) {
-                    openWith = "cart";
-
-                    toggleNav("cart");
-                    const icons = header?.querySelectorAll("svg");
-                    icons?.forEach((icon) => {
-                        if (icon.id === "CloseIcon") {
-                            icon.classList.toggle("hidden");
-                            console.log(icon.id);
-                        } else if (icon.id === "BarsIcon") {
-                            icon.classList.toggle("hidden");
-                            console.log(icon.id);
-                        }
-                    });
-                } else {
-                    if (openWith === "menu") {
-                        toggleMiniCart();
-                        openWith = "cart";
-                    } else if (openWith === "cart") {
-                        toggleNav("cart");
-                    }
+                    toggleMiniCart(button.id);
+                    setTimeout(() => {
+                        toggleNav();
+                        toggleHammyIcon();
+                    }, 100);
+                } else if (isOpen) {
+                    setTimeout(() => {
+                        toggleMiniCart(button.id);
+                        toggleNavItems(button.id);
+                    }, 100);
                 };
-
-                cartMenuBtn?.classList.toggle("hidden");
-                goToMenuButton?.classList.toggle("hidden");
-                goToMenuButton?.classList.add("flex");
 
                 break;
             }
 
-            case "cartMenuButton": {
-                toggleMiniCart();
+            case "modalCartButton": {
+                toggleMiniCart(button.id);
 
-                button.classList.toggle("hidden");
-                goToMenuButton?.classList.toggle("hidden");
-                goToMenuButton?.classList.add("flex");
+                buttons.forEach((button) => {
+                    if (button.id === "cartButton") {
+                        button.focus();
+                    }
+                });
 
                 break;
             };
 
-            case "goToMenuButton": {
-                toggleNavItems();
-
-                button.classList.toggle("hidden");
-                cartMenuBtn?.classList.toggle("hidden");
+            case "modalMenuButton": {
+                toggleNavItems(button.id);
 
                 break;
             };
@@ -81,20 +76,12 @@ buttons.forEach((button) => {
     });
 });
 
-const toggleNav = (option: string) => {
+const toggleNav = () => {
     if (!isOpen) {
         isOpen = true;
         nav?.classList.add("nav-in");
         nav?.classList.remove("nav-out");
         nav?.classList.remove("hidden");
-
-        if (option === "cart") {
-            menuItems?.classList.add("hidden");
-            miniCart?.classList.remove("hidden");
-        } else if (option === "menu") {
-            menuItems?.classList.remove("hidden");
-            miniCart?.classList.add("hidden");
-        }
 
     } else {
         isOpen = false;
@@ -106,26 +93,72 @@ const toggleNav = (option: string) => {
     }
 };
 
-const toggleMiniCart = () => {
-    menuItems?.classList.toggle("nav-out");
-    miniCart?.classList.toggle("nav-in");
+const toggleMiniCart = (button: string) => {
+    if (button === "cartButton") {
+        modalMenuButton?.classList.toggle("hidden");
+        miniCart?.classList.toggle("nav-in");
+        miniCart?.classList.toggle("hidden");
 
-    setTimeout(() => {
-        miniCart?.classList.remove("hidden");
-        menuItems?.classList.add("hidden");
-        menuItems?.classList.remove("nav-in");
-    }, 100);
+    } else if (button === "modalCartButton") {
+        // Buttons
+        modalMenuButton?.classList.toggle("hidden");
+        modalCartButton?.classList.toggle("hidden");
+
+        menuItems?.classList.toggle("nav-out");
+        miniCart?.classList.toggle("nav-in");
+        // Cart
+        setTimeout(() => {
+            miniCart?.classList.remove("hidden");
+            menuItems?.classList.add("hidden");
+            menuItems?.classList.remove("nav-in");
+        }, 100);
+    };
+
+    isMiniCartOpen = true;
+    isMenuItemsOpen = false;
+    console.log("MenuItems: ", isMenuItemsOpen);
+    console.log("MiniCart: ", isMiniCartOpen);
 };
 
-const toggleNavItems = () => {
-    miniCart?.classList.toggle("nav-in");
-    miniCart?.classList.toggle("nav-out");
-    menuItems?.classList.toggle("nav-out");
-    menuItems?.classList.toggle("nav-in");
+const toggleNavItems = (button: string) => {
+    if (button === "menuButton") {
+        menuItems?.classList.toggle("nav-in");
+        menuItems?.classList.toggle("hidden");
+        menuItems?.classList.remove("nav-out");
+        setTimeout(() => {
+            modalCartButton?.classList.toggle("hidden");
+        }, 100);
+    } else if (button === "modalMenuButton") {
+        // Buttons
+        modalCartButton?.classList.toggle("hidden");
+        modalMenuButton?.classList.toggle("hidden");
+        menuItems?.classList.remove("nav-out");
 
-    setTimeout(() => {
-        menuItems?.classList.remove("hidden");
-        miniCart?.classList.add("hidden");
-        miniCart?.classList.remove("nav-out");
-    }, 100);
+        // MenuItems
+        menuItems?.classList.toggle("nav-in");
+        miniCart?.classList.toggle("nav-out");
+        setTimeout(() => {
+            menuItems?.classList.remove("hidden");
+            miniCart?.classList.add("hidden");
+            miniCart?.classList.remove("nav-out");
+        }, 100);
+    } else if (button === "cartButton") {
+        console.log("Boton de carrito presionado cuando el menu esta abierto");
+        toggleNavItems("menuButton");
+    };
+
+    isMenuItemsOpen = true;
+    isMiniCartOpen = false;
+    console.log("MenuItems: ", isMenuItemsOpen);
+    console.log("MiniCart: ", isMiniCartOpen);
+};
+
+const toggleHammyIcon = () => {
+    icons?.forEach((icon) => {
+        if (icon.id === "CloseIcon") {
+            icon.classList.toggle("hidden");
+        } else if (icon.id === "BarsIcon") {
+            icon.classList.toggle("hidden");
+        }
+    });
 };
